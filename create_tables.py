27651ea6a03b2dccde49c0f8077fdff4ebc9ab1d
dnbabkov <83114973+dnbabkov@users.noreply.github.com>
2026-04-29 16:02:30 +0300

    Moved backend to its own directory to make way for the frontend code. README is now a README

--- create_tables.py
+++
@@ -1,8 +0,0 @@
-from lit_club_app.db.base import Base
-from lit_club_app.db.session import engine
-from lit_club_app.users.models import User
-from lit_club_app.books.models import Book
-from lit_club_app.meetings.models import Meeting
-from lit_club_app.selections.models import BookSelection, Vote, Nomination
-
-Base.metadata.create_all(bind=engine)
